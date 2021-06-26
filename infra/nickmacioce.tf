1333141cba55ab641c4239f3a31d5c53ee967190
Nick Macioce <ndmacioce@gmail.com>
2021-06-26 11:53:00 -0600

    feat: add additional static site

--- infra/nickmacioce.tf
+++ infra/nickmacioce.tf
@@ -4,3 +4,10 @@ module "aws_static_website" {
   website-domain-main     = "nickmacioce.com"
   website-domain-redirect = "www.nickmacioce.com"
 }
+
+module "lilmacioce_static_website" {
+  source = "cloudmaniac/static-website/aws"
+
+  website-domain-main     = "isbabymaciocehereyet.com"
+  website-domain-redirect = "www.isbabymaciocehereyet.com"
+}
