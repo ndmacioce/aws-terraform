ec802f30c28b8adb00599c9a967a71a2219e9461
Nick Macioce <ndmacioce@gmail.com>
2021-08-14 17:45:13 -0600

    ci: remove sites

--- infra/nickmacioce.tf
+++ infra/nickmacioce.tf
@@ -1,20 +1,13 @@
-module "aws_static_website" {
-  source = "cloudmaniac/static-website/aws"
+# module "aws_static_website" {
+#   source = "cloudmaniac/static-website/aws"
 
-  website-domain-main     = "nickmacioce.com"
-  website-domain-redirect = "www.nickmacioce.com"
-}
+#   website-domain-main     = "nickmacioce.com"
+#   website-domain-redirect = "www.nickmacioce.com"
+# }
 
-module "lilmacioce_static_website" {
-  source = "cloudmaniac/static-website/aws"
+# module "lilmacioce_static_website" {
+#   source = "cloudmaniac/static-website/aws"
 
-  website-domain-main     = "isbabymaciocehereyet.com"
-  website-domain-redirect = "www.isbabymaciocehereyet.com"
-}
-
-module "aws_static_website_3" {
-  source = "../modules/static-site"
-
-  hosted-zone-name    = "nickmacioce.com" # UPDATE ME WITH CORRECT HOSTED ZONE!!
-  website-domain-main = "links.nickmacioce.com" # UPDATE ME WITH DOMAIN NAME. NOTE THIS MAY OR MAY NOT BE THE SAME AS THE HOSTED ZONE DEPENDING ON ENV.
-}
+#   website-domain-main     = "isbabymaciocehereyet.com"
+#   website-domain-redirect = "www.isbabymaciocehereyet.com"
+# }
--- modules/static-site/output.tf
+++
@@ -1,9 +0,0 @@
-output "website_cdn_root_id" {
-  description = "Main CloudFront Distribution ID"
-  value       = aws_cloudfront_distribution.website_cdn_root.id
-}
-
-output "website_root_s3_bucket" {
-  description = "The website root bucket where resources are uploaded"
-  value       = aws_s3_bucket.website_root.bucket
-}
