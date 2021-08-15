5e3573df1200b26768bd49a824525eaa5d869cae
Nick Macioce <ndmacioce@gmail.com>
2021-08-14 22:24:51 -0600

    feat: readd sites

--- infra/nickmacioce.tf
+++ infra/nickmacioce.tf
@@ -1,13 +1,34 @@
-module "aws_static_website" {
-  source = "cloudmaniac/static-website/aws"
+# module "aws_static_website" {
+#   source = "cloudmaniac/static-website/aws"
 
-  website-domain-main     = "nickmacioce.com"
-  website-domain-redirect = "www.nickmacioce.com"
+#   website-domain-main     = "nickmacioce.com"
+#   website-domain-redirect = "www.nickmacioce.com"
+# }
+
+# module "lilmacioce_static_website" {
+#   source = "cloudmaniac/static-website/aws"
+
+#   website-domain-main     = "isbabymaciocehereyet.com"
+#   website-domain-redirect = "www.isbabymaciocehereyet.com"
+# }
+
+module "nickmacioce" {
+  source = "../modules/site-infra"
+
+  hosted-zone-name    = "nickmacioce.com" # UPDATE ME WITH CORRECT HOSTED ZONE!!
+  website-domain-main = "nickmacioce.com" # UPDATE ME WITH DOMAIN NAME. NOTE THIS MAY OR MAY NOT BE THE SAME AS THE HOSTED ZONE DEPENDING ON ENV.
 }
 
-module "lilmacioce_static_website" {
-  source = "cloudmaniac/static-website/aws"
+module "links-nickmacioce" {
+  source = "../modules/site-infra"
 
-  website-domain-main     = "isbabymaciocehereyet.com"
-  website-domain-redirect = "www.isbabymaciocehereyet.com"
+  hosted-zone-name    = "nickmacioce.com"       # UPDATE ME WITH CORRECT HOSTED ZONE!!
+  website-domain-main = "links.nickmacioce.com" # UPDATE ME WITH DOMAIN NAME. NOTE THIS MAY OR MAY NOT BE THE SAME AS THE HOSTED ZONE DEPENDING ON ENV.
 }
+
+# module "isbabymaciocehereyet" {
+#   source = "../modules/site-infra"
+
+#   hosted-zone-name    = "isbabymaciocehereyet.com" # UPDATE ME WITH CORRECT HOSTED ZONE!!
+#   website-domain-main = "isbabymaciocehereyet.com" # UPDATE ME WITH DOMAIN NAME. NOTE THIS MAY OR MAY NOT BE THE SAME AS THE HOSTED ZONE DEPENDING ON ENV.
+# }
--- infra/route-53.tf
+++ infra/route-53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_zone" "baby-macioce" {
     Environment = "prod"
   }
 }
+
+resource "aws_route53_zone" "nickmacioce" {
+  name = "nickmacioce.com"
+
+  tags = {
+    Environment = "prod"
+  }
+}
