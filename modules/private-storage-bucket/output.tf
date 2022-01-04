153f488431542b23eff99ce5c0f29e0c8730fe48
Nick Macioce <ndmacioce@gmail.com>
2022-01-04 09:42:09 -0700

    fix: private s3 module

--- modules/private-storage-bucket/output.tf
+++ modules/private-storage-bucket/output.tf
@@ -1,4 +1,4 @@
-output "website_root_s3_bucket" {
+output "s3_bucket" {
   description = "The website root bucket where resources are uploaded"
-  value       = aws_s3_bucket.website_root.bucket
+  value       = aws_s3_bucket.storage_bucket.bucket
 }
